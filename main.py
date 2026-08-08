cdb5b169a1924d4498cdcfeebf3df440a37e9ba7
Mohammed Akram <nerddude9000@proton.me>
2026-08-08 01:47:52 +0100

    docs: add more todos and format

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import os
 
 from PySide6.QtWidgets import QApplication, QFileDialog, QMainWindow, QMessageBox
@@ -7,7 +6,8 @@ from ui_mainwindow import Ui_MainWindow
 from ydl import DownloadType, download
 
 # TODO: The ui is too small, implement a real fix
-os.environ["QT_SCALE_FACTOR"] = "1.5" 
+os.environ["QT_SCALE_FACTOR"] = "1.5"
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -17,51 +17,53 @@ class MainWindow(QMainWindow):
         self.setup_app()
 
     def setup_app(self):
-        self.ui.pathButton.clicked.connect(
-            lambda: self.update_download_path()
-        )
-        
-        self.ui.downloadButton.clicked.connect(
-            lambda: self.start_download()
-        )
+        # TODO: load download path from config file
+        self.ui.pathButton.clicked.connect(lambda: self.update_download_path())
+
+        self.ui.downloadButton.clicked.connect(lambda: self.start_download())
 
     def start_download(self):
         url = self.ui.urlInput.text()
 
         # TODO: maybe check with ytdlp?
         if len(url) == 0:
-            QMessageBox.information(self, "هناك خلل", "أدخلوا رابط المقطع أو قائمة التشغيل أولا.")
+            QMessageBox.information(
+                self, "هناك خلل", "أدخلوا رابط المقطع أو قائمة التشغيل أولا."
+            )
             return
-        
+
         download_location = self.ui.pathLabel.text()
 
         if not os.path.exists(download_location):
-            QMessageBox.information(self, "هناك خلل", "مجلد الخزانة الذي اخترتموه غير صحيح، قم بتغييره أولا.")
+            QMessageBox.information(
+                self,
+                "هناك خلل",
+                "مجلد الخزانة الذي اخترتموه غير صحيح، قم بتغييره أولا.",
+            )
             return
 
         download_type: DownloadType
-    
+
         if self.ui.downloadModeRadio_Audio.isChecked():
             download_type = DownloadType.m4a
         elif self.ui.downloadModeRadio_720p.isChecked():
             download_type = DownloadType["720p"]
         else:
             download_type = DownloadType.best
-    
-    
+
         download(url, download_type, download_location)
-    
+
     def update_download_path(self):
+        # TODO: save to a config file
         folder = QFileDialog.getExistingDirectory(self, "أين تريد تنزيل المقاطع؟")
         if folder:
             self.ui.pathLabel.setText(folder)
-    
+
 
 if __name__ == "__main__":
     app = QApplication()
-    
+
     window = MainWindow()
     window.show()
 
     app.exec()
-
--- ydl.py
+++ ydl.py
@@ -8,6 +8,7 @@ from yt_dlp import YoutubeDL
 FFMPEG_BINARY_PATH = "./vendor/ffmpeg/ffmpeg"
 DownloadType = Enum("DownloadType", ["m4a", "720p", "best"])
 
+
 # TODO: Use FFMPEG_BINARY_PATH
 # TODO: Show progress
 # TODO: Show success and error message boxes
@@ -18,7 +19,11 @@ def download(url: str, download_type: DownloadType, download_location: str):
         dl_format = "bv[height=720]+(139/ba/m4a)"
     else:
         dl_format = "bv+ba"
-        
-    with YoutubeDL(params={"format": dl_format, "outtmpl": path.join(download_location, "%(title)s%(ext)s")}) as dl:
-        dl.download([url]) # needs a list
 
+    with YoutubeDL(
+        params={
+            "format": dl_format,
+            "outtmpl": path.join(download_location, "%(title)s%(ext)s"),
+        }
+    ) as dl:
+        dl.download([url])  # needs a list
