a5eadb0a11503da16dbd42633e93dbe8c5faf2d0
Mohammed Akram <nerddude9000@proton.me>
2026-08-08 11:39:52 +0100

    feat: change yt-dlp interface into a worker thread

--- ydl.py
+++ ydl.py
@@ -1,7 +1,9 @@
 # This is a basic interface for ytdlp just to make things simpler
+import typing
 from enum import Enum
 from os import path
 
+from PySide6.QtCore import QThread, Signal
 from yt_dlp import YoutubeDL
 
 # TODO: Set it based on OS
@@ -9,21 +11,46 @@ FFMPEG_BINARY_PATH = "./vendor/ffmpeg/ffmpeg"
 DownloadType = Enum("DownloadType", ["m4a", "720p", "best"])
 
 
-# TODO: Use FFMPEG_BINARY_PATH
-# TODO: Show progress
-# TODO: Show success and error message boxes
-def download(url: str, download_type: DownloadType, download_location: str):
-    if download_type == DownloadType["m4a"]:
-        dl_format = "139/ba/m4a"
-    elif download_type == DownloadType["720p"]:
-        dl_format = "bv[height=720]+(139/ba/m4a)"
-    else:
-        dl_format = "bv+ba"
+class DownloadWorker(QThread):
+    progress_signal = Signal(dict)
+    finish_signal = Signal()
+    options: typing.ClassVar
 
-    with YoutubeDL(
-        params={
-            "format": dl_format,
-            "outtmpl": path.join(download_location, "%(title)s%(ext)s"),
-        }
-    ) as dl:
-        dl.download([url])  # needs a list
+    def __init__(
+        self,
+        url: str,
+        download_type: DownloadType,
+        download_location: str,
+    ) -> None:
+        super().__init__()
+        self.options.url = url
+        self.options.download_type = download_type
+        self.options.download_location = download_location
+
+    # TODO: Use FFMPEG_BINARY_PATH
+    # TODO: File path handling for playlists (such as creating a new folder for them)
+    def run(self):
+        op = self.options
+
+        if op.download_type == DownloadType["m4a"]:
+            dl_format = "139/ba/m4a"
+        elif op.download_type == DownloadType["720p"]:
+            dl_format = "bv[height=720]+(139/ba/m4a)"
+        else:
+            dl_format = "bv+ba"
+
+        with YoutubeDL(
+            params={
+                "format": dl_format,
+                "outtmpl": path.join(op.download_location, "%(title)s%(ext)s"),
+                "progress_hooks": op.progress_hooks,
+            }
+        ) as dl:
+            try:
+                error_code = dl.download([op.url])  # needs a list
+                self.finish_signal.emit(error_code)
+            except:  # noqa: E722
+                self.finish_signal.emit(1)
+
+    def _hook(self, data):
+        self.progress_signal.emit(data)
