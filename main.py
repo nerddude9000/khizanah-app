74757d7c3cd0fec1f9b89a6884af31593229815c
Mohammed Akram <nerddude9000@proton.me>
2026-08-08 12:04:56 +0100

    feat: switch to threaded download, and add progress text

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from PySide6.QtWidgets import QApplication, QFileDialog, QMainWindow, QMessageBox
 
 from ui_mainwindow import Ui_MainWindow
-from ydl import DownloadType, download
+from ydl import DownloadType, DownloadWorker
 
 # TODO: The ui is too small, implement a real fix
 os.environ["QT_SCALE_FACTOR"] = "1.5"
@@ -22,10 +22,40 @@ class MainWindow(QMainWindow):
 
         self.ui.downloadButton.clicked.connect(lambda: self.start_download())
 
+    def on_progress_download(self, data):
+        status = data["status"]
+
+        # TODO: display different text for audio and video parts?
+        if status == "finished":
+            self.ui.infoLabel.setText("انتهى التحميل.")
+        elif status == "error":
+            self.ui.infoLabel.setText("حدث خلل أثناء التحميل.")
+        elif status == "downloading":
+            try:
+                progress_percentage = round(
+                    (data["downloaded_bytes"] / data["total_bytes"]) * 100
+                )
+
+                self.ui.infoLabel.setText(f"جاري التحميل... {progress_percentage}%")
+            except:  # noqa: E722
+                self.ui.infoLabel.setText(
+                    "جاري التحميل... (لم نستطع استخراج مدى اكتمال التحميل)."
+                )
+
+    def on_finish_download(self, err_code: int):
+        if err_code:
+            QMessageBox.information(
+                self,
+                "هناك خلل",
+                f"حدث خلل أثناء التحميل ({err_code})\nتأكدوا من الرابط الذي أدخلتموه، ومن الاتصال بالشبكة.",
+            )
+        else:
+            # TODO: Add button for opening the file or at least the containing folder
+            QMessageBox.information(self, "تمت العملية بنجاح", "تم تحميل المقطع بنجاح")
+
     def start_download(self):
         url = self.ui.urlInput.text()
 
-        # TODO: maybe check with ytdlp?
         if len(url) == 0:
             QMessageBox.information(
                 self, "هناك خلل", "أدخلوا رابط المقطع أو قائمة التشغيل أولا."
@@ -51,7 +81,16 @@ class MainWindow(QMainWindow):
         else:
             download_type = DownloadType.best
 
-        download(url, download_type, download_location)
+        # We must use self to prevent it from being garbage collected
+        self.worker = DownloadWorker(
+            url,
+            download_type,
+            download_location,
+        )
+        self.worker.progress_signal.connect(self.on_progress_download)
+        self.worker.finish_signal.connect(self.on_finish_download)
+        self.ui.infoLabel.setText("انتظروا قليلا حتى يبدأ التحميل...")
+        self.worker.start()
 
     def update_download_path(self):
         # TODO: save to a config file
--- ydl.py
+++ ydl.py
@@ -1,5 +1,4 @@
 # This is a basic interface for ytdlp just to make things simpler
-import typing
 from enum import Enum
 from os import path
 
@@ -11,10 +10,14 @@ FFMPEG_BINARY_PATH = "./vendor/ffmpeg/ffmpeg"
 DownloadType = Enum("DownloadType", ["m4a", "720p", "best"])
 
 
+#
+# NOTE: This used to be a single sync function instead of a thread, which caused
+# rendering to stop until the entire download finished or erred, which therefore
+# progress elements to not show, and the app itself to freeze.
+#
 class DownloadWorker(QThread):
     progress_signal = Signal(dict)
-    finish_signal = Signal()
-    options: typing.ClassVar
+    finish_signal = Signal(int)
 
     def __init__(
         self,
@@ -23,18 +26,20 @@ class DownloadWorker(QThread):
         download_location: str,
     ) -> None:
         super().__init__()
-        self.options.url = url
-        self.options.download_type = download_type
-        self.options.download_location = download_location
+        self.options = {
+            "url": url,
+            "download_type": download_type,
+            "download_location": download_location,
+        }
 
     # TODO: Use FFMPEG_BINARY_PATH
     # TODO: File path handling for playlists (such as creating a new folder for them)
     def run(self):
         op = self.options
 
-        if op.download_type == DownloadType["m4a"]:
+        if op["download_type"] == DownloadType["m4a"]:
             dl_format = "139/ba/m4a"
-        elif op.download_type == DownloadType["720p"]:
+        elif op["download_type"] == DownloadType["720p"]:
             dl_format = "bv[height=720]+(139/ba/m4a)"
         else:
             dl_format = "bv+ba"
@@ -42,12 +47,12 @@ class DownloadWorker(QThread):
         with YoutubeDL(
             params={
                 "format": dl_format,
-                "outtmpl": path.join(op.download_location, "%(title)s%(ext)s"),
-                "progress_hooks": op.progress_hooks,
+                "outtmpl": path.join(op["download_location"], "%(title)s.%(ext)s"),
+                "progress_hooks": [self._hook],
             }
         ) as dl:
             try:
-                error_code = dl.download([op.url])  # needs a list
+                error_code = dl.download([op["url"]])  # needs a list
                 self.finish_signal.emit(error_code)
             except:  # noqa: E722
                 self.finish_signal.emit(1)
