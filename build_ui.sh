911e2179ee6f76118035e8ef5097e946995772aa
Mohammed Akram <nerddude9000@proton.me>
2026-08-09 16:15:51 +0100

    redesign: bigger ui

--- build_ui.sh
+++ build_ui.sh
@@ -2,5 +2,5 @@
 
 source env/bin/activate
 echo "compiling assets/gui.ui to ui_mainwindow.py..."
-env/bin/pyside6-uic assets/gui.ui -o ui_mainwindow.py
+pyside6-uic assets/gui.ui -o ui_mainwindow.py
 echo "Done."
